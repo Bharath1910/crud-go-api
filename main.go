b9bd23e8fdadb30c24e045e0ead4491a1bce03b3
Bharath1910 <av.bharath@proton.me>
2023-02-20 17:40:00 +0530

    completed boilerplate code TODO: 1. Complete getMovies method 2. Complete getMovies by id method 3. Complete createMovies method 4. Complete updateMovies method 5. Complete deleteMoviles method

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
+	r.HandleFunc("/moviles/{id}", deleteMovie).Methods("DELETE")
+
+	http.ListenAndServe(":8000", r)
 }
